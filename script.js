d6e97849f6b2b69c93eea51d36fe137ce54eb9a2
Michel Maia <programador.michel.maia@gmail.com>
2023-01-26 18:52:38 -0300

    commit

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-async function buscaEnd() {
-    var consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json')
+async function buscaEnd(cep) {
+
+    try { 
+    var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`)
     var consultaCEPConvertida = await consultaCEP.json();
 
+    if (consultaCEPConvertida.erro) {
+        throw Error('CEP não existe');
+    }
+
     console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+
+    } catch (erro) {
+        console.log(erro)
+    }
+
 }
 
-buscaEnd();
+let ceps = ['01001000','01001001'];
+let conjCeps = ceps.map(valores => buscaEnd(valores));
+Promise.all(conjCeps).then(respostas => console.log(respostas));
+
 
 
 
