17444cd7e65b14f543ebb6a1e89b6884b4aa6d62
Michel Maia <programador.michel.maia@gmail.com>
2023-01-26 19:20:56 -0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 async function buscaEnd(cep) {
 
+    var msgErro = document.getElementById('erro');
+    msgErro.innerHTML = "";
     try { 
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`)
     var consultaCEPConvertida = await consultaCEP.json();
@@ -7,19 +9,26 @@ async function buscaEnd(cep) {
     if (consultaCEPConvertida.erro) {
         throw Error('CEP não existe');
     }
+    var cidade = document.getElementById('cidade');
+    var logradouro = document.getElementById('endereco');
+    var estado = document.getElementById('estado');
+     
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
 
     console.log(consultaCEPConvertida);
     return consultaCEPConvertida;
 
     } catch (erro) {
+        msgErro.innerHTML = `<p> CEP inválido. Tente novamente!</p>`
         console.log(erro)
     }
 
 }
 
-let ceps = ['01001000','01001001'];
-let conjCeps = ceps.map(valores => buscaEnd(valores));
-Promise.all(conjCeps).then(respostas => console.log(respostas));
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEnd(cep.value));
 
 
 
